148408d1afa7e34ae89ecf2dc12d1f2a2271a91d
bstankovits <bstankov@mit.edu>
2023-02-21 06:39:41 +0000

    initial commit

---
+++ simple_publisher.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# license removed for brevity
+
+import rospy
+from std_msgs.msg import Float32
+import random
+
+def simple_publisher():
+	pub = rospy.Publisher('my_random_float', Float32, queue_size = 10)
+	rospy.init_node('simple_publisher', anonymous = True)
+	rate = rospy.Rate(20)
+	while not rospy.is_shutdown():
+		num = random.uniform(0.0, 10.0)
+		rospy.loginfo(num)
+		pub.publish(num)
+		rate.sleep()
+
+if __name__ == '__main__':
+	try:
+		simple_publisher()
+	except rospy.ROSInterruptException:
+		pass
